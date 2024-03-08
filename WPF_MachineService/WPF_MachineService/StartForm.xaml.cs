6898f95ba852297798d34aa39ed8b62f55d483eb
CaoKienQuoc <caokienquoc1582002@gmail.com>
2024-03-08 09:12:19 +0700

    FirstCommit

---
+++ WPF_MachineService/WPF_MachineService/StartForm.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_MachineService
+{
+    /// <summary>
+    /// Interaction logic for StartForm.xaml
+    /// </summary>
+    public partial class StartForm : Window
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
